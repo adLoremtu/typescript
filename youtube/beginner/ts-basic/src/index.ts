99128d390c80229a6db3c8899d06cd9e75201a75
takuya inoue <takuya.inoue@nijibox.co.jp>
2022-01-10 18:42:57 +0900

    05_function

--- youtube/beginner/ts-basic/src/index.ts
+++ youtube/beginner/ts-basic/src/index.ts
@@ -6,9 +6,20 @@
 // world.sayHello(root)
 
 // 03.基本の型定義
-import { unknownSample, anySample, notExistSample, primitiveSample } from './basic'
+// import { unknownSample, anySample, notExistSample, primitiveSample } from './basic'
 
-primitiveSample()
-notExistSample()
-anySample()
-unknownSample()
+// primitiveSample()
+// notExistSample()
+// anySample()
+// unknownSample()
+
+// 04.関数の型定義
+import { logMessage } from './function/basic'
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
+
+logMessage('Hello TypeScript')
+isUserSignedIn('ABC', 'Torahack')
+isUserSignedIn('DEF')
+isUserSignedIn2('ABC')
+const sum = sumProductsPrice(100, 200, 300, 400, 500)
+console.log('Function parameters sample 5: ', sum)
